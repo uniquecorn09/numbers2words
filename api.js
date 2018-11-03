5cb21e328b657b978b8619105dae60d3840d6831
Jenny Stephan <jenny.stephan@de.ibm.com>
2018-11-03 18:55:30 +0100

    refactored

--- api.js
+++ api.js
@@ -4,17 +4,11 @@ const initRoutes = app => {
 };
 
 const invokeConverter = (req, res) => {
-  console.log(req.body);
-  console.log(Converter)
   let number2Convert = req.body.number;
-
   let response = Converter.convert(number2Convert);
   res.header("Content-Type", "application/json");
   res.send(JSON.stringify({response:response}));
   };
-
-
-require("dotenv").config();
 
 module.exports = {
     init: initRoutes
--- converter.js
+++ converter.js
@@ -1,26 +1,24 @@
 const atomicNumbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fivteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-const tenthExceptions = [ false, "twen", "thir", "for", "fif", false, false, "eigh", false]
-const tensPotency = ["hundred", "thousand", "million", "billion"]
+const tensDigits = [ "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+const tensPotency = ["hundred", "thousand", "million", "billion", "trillion"]
 const BASE = 10;
-
 
 const combineNumberWords = (num) => {
     let numberWord =  "";
     let digitCount = num.toString().length;
     let op = Math.pow(BASE, digitCount-1);
-    let digit = Math.floor(num/op);
-    let next = num%op;
+    let firstDigit = Math.floor(num/op);
+    let restDigits = num%op;
 
     if(digitCount < 3){
         if(num < 20)
             numberWord = atomicNumbers[num]
         else
-            numberWord = (tenthExceptions[digit-1] ? tenthExceptions[digit-1] : atomicNumbers[digit])+ "ty" + (next != 0 ? "-"+atomicNumbers[next]: "")
-
+            numberWord = tensDigits[firstDigit-2] + (restDigits != 0 ? "-"+atomicNumbers[restDigits]: "")
     }
 
     if(digitCount === 3){
-        numberWord = atomicNumbers[digit]+" "+tensPotency[digitCount-3]+ (next != 0 ? (" and "+combineNumberWords(next)) : "");
+        numberWord = atomicNumbers[firstDigit] + " " + tensPotency[digitCount-3] + (restDigits != 0 ? (" and "+combineNumberWords(restDigits)) : "");
     }
 
     if (digitCount > 3){
@@ -29,11 +27,12 @@ const combineNumberWords = (num) => {
         let div = Math.pow(BASE, pow);
         let prefix = Math.floor(num/div);
         let suffix = num%div;
-        numberWord = combineNumberWords(prefix)+" "+tensPotency[fak] + (suffix != 0 ? " "+combineNumberWords(suffix) : "");
+        numberWord = combineNumberWords(prefix) + " " + tensPotency[fak] + (suffix != 0 ? " "+combineNumberWords(suffix) : "");
     }
     return numberWord;
 }
-const numberToWord = (num) => {
+
+const convert = (num) => {
     console.log("Converting " +num+" ...")
     let result = "";
     if(num < 0 ){
@@ -43,9 +42,8 @@ const numberToWord = (num) => {
     result += combineNumberWords(num);
     console.log("RESULT: "+result);
     return result;
-    
 }
 
 module.exports = {
-    convert: numberToWord
+    convert: convert
   };
