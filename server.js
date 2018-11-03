b64253214f1119261af5b12ea4523e6f8ff51fbd
Jenny Stephan <jenny.stephan@de.ibm.com>
2018-11-03 18:55:30 +0100

    refactored

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const bodyParser = require("body-parser");
 const api = require("./api");
 
 const app = express();
+require("dotenv").config();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("./client"));
- api.init(app);
+api.init(app);
 
 app.listen(process.env.PORT, () =>
   console.log(
--- src/app.js
+++ src/app.js
@@ -1,25 +1,37 @@
+const convert = () => {
+    let number = document.getElementById('number').value;
+    if(!parseInt(number)){
+        printResult("Please insert a number")
+        return;
+    }
+    let body = {number: number};
+    const headers = new Headers({
+        "Content-Type": "application/json",
+        "Content-Length": JSON.stringify(body).length
+    })
+    fetch('/convert', {
+        method: 'POST',
+        headers : headers,
+        body:JSON.stringify(body)
+    }).then((res) => {
+        res.json().then((data) =>  {
+            printResult(data.response)
+        });
+    });
+}
 
+const printResult = (string) => {
+    let res = document.getElementById("result");
+    res.innerHTML = string;
+}
 
 document.addEventListener("DOMContentLoaded",function(){
     let button = document.getElementById("submit")
-    console.log(button)
-    button.addEventListener("click", (event) => {
-        let number = document.getElementById('number').value;
-        let body = {number: number};
-        const headers = new Headers({
-            "Content-Type": "application/json",
-            "Content-Length": JSON.stringify(body).length
-        })
-        console.log(JSON.stringify(body))
-        fetch('/convert', {
-            method: 'POST',
-            headers : headers,
-            body:JSON.stringify(body)
-        }).then((res) => {
-            res.json().then((data) =>  {
-                let res = document.getElementById("result");
-                res.innerHTML = data.response;
-            });
-        });
+    button.addEventListener("click", convert)
+    let input = document.getElementById("number");
+    input.addEventListener("keydown", (event) => {
+        if(event.keyCode === 13){
+            convert();
+        }
     })
 });
